64e13a7d4ecd76054d76a9def45b57f5b8e2e621
Michael Shihjay Chen <shihjay2@gmail.com>
2016-08-02 22:23:23 -0700

    Increased size of client secret table

---
+++ database/migrations/2016_08_03_052138_change_client_secret_in_oauth_clients_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeClientSecretInOauthClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('oauth_clients', function (Blueprint $table) {
+            $table->string('client_secret', 255)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('oauth_clients', function (Blueprint $table) {
+            $table->string('client_secret', 80)->change();
+        });
+    }
+}
