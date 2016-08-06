bff8ff449ab85b31770b01d5b66c2e625b365051
Michael Shihjay Chen <shihjay2@gmail.com>
2016-08-06 14:57:49 -0700

    Updated login form.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,7 +64,7 @@
 									<i class="fa fa-btn fa-sign-in"></i> Login
 								</button>
 
-								<a class="btn btn-link" href="{{ url('/password/_email') }}">Forgot Your Password?</a>
+								<a class="btn btn-link" href="{{ url('/password_email') }}">Forgot Your Password?</a>
 							</div>
 						</div>
 						@if (!isset($nooauth))
