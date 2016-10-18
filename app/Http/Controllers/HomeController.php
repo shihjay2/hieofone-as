3d8f86f82d5f4fbd2b2d1b45d630c35de35c3d41
Michael Shihjay Chen <shihjay2@gmail.com>
2016-10-18 05:54:05 -0700

    Updated default policy page.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -671,6 +671,7 @@ class HomeController extends Controller
     {
         $data['message_action'] = $request->session()->get('message_action');
         $request->session()->forget('message_action');
+        $data['name'] = $request->session()->get('owner');
         $data['content'] = '<div><i class="fa fa-child fa-5x" aria-hidden="true" style="margin:20px;text-align: center;"></i></div>';
         $data['content'] .= '<h3>Resource Registration Consent Default Policies</h3>';
         $data['content'] .= '<p>You can set default policies (who gets access to your resources) whenever you have a new resource server registered to this authorization server.</p>';
