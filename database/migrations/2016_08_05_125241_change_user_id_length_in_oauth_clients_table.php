48af4179f11784c155f61cdf5fa59e6cd4e302e4
Michael Shihjay Chen <shihjay2@gmail.com>
2016-10-29 16:25:09 -0700

    Fixed duplicate class name for migrations.

--- database/migrations/2016_08_05_125241_change_user_id_length_in_oauth_clients_table.php
+++ database/migrations/2016_08_05_125241_change_user_id_length_in_oauth_clients_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeUserIdInOauthClientsTable extends Migration
+class ChangeUserIdLengthInOauthClientsTable extends Migration
 {
     /**
      * Run the migrations.
