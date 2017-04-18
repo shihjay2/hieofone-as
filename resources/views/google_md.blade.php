be0d0d3de9cf0f87bc5a511c451ba0f38889d033
Michael Shihjay Chen <shihjay2@gmail.com>
2017-04-17 23:43:49 -0700

    Edit google_md page layout

--- resources/views/google_md.blade.php
+++ resources/views/google_md.blade.php
@@ -7,11 +7,14 @@
 			<div class="panel panel-default">
 				<div class="panel-heading">Additional information to access {{ $owner }}</div>
 				<div class="panel-body">
+					<div style="text-align: center;">
+						<h4>Choose the NPI that matches your identity:</h4>
+					</div>
 					{!! $npi !!}
 					<form class="form-horizontal" role="form" method="POST" action="{{ url('/google_md') }}">
 						{{ csrf_field() }}
 						<div style="text-align: center;">
-							<h4>Enter your NPI number below if you don't see one that matches in the list.</h4>
+							<h4>Or enter your NPI number below if you don't see one that matches in the list.</h4>
 						</div>
 						<br>
 						<div class="form-group{{ $errors->has('npi') ? ' has-error' : '' }}">
