ee77564f0287dda6e37cc6bff226a3ba7977d9a4
Michael Shihjay Chen <shihjay2@gmail.com>
2018-07-31 07:34:52 -0700

    Update system

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -977,7 +977,7 @@ class OauthController extends Controller
                                         }
                                     }
                                     file_put_contents($filename, $file);
-                                    if ($filename == 'composer.json' || $filename == 'composer.lock') {
+                                    if ($row1['filename'] == 'composer.json' || $row1['filename'] == 'composer.lock') {
                                         $composer = true;
                                     }
                                 }
